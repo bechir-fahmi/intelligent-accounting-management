fd05b265308eeeb91cb722b615611f7436b68720
bechir-fahmi <95341647+bechir-fahmi@users.noreply.github.com>
2025-03-21 14:37:58 +0100

    Add AuthController to handle user registration and login functionality with appropriate guards and DTOs

---
+++ backend/src/modules/auth/auth.controller.ts
@@ -0,0 +1,22 @@
+import { Body, Controller, HttpCode, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from '../users/dto/create-user.dto';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+import { LoginDto } from './dtos/login.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  async register(@Body() createUserDto: CreateUserDto) {
+    return this.authService.register(createUserDto);
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  @HttpCode(200)
+  async login(@Request() req, @Body() loginDto: LoginDto) {
+    return this.authService.login(req.user);
+  }
+} 
