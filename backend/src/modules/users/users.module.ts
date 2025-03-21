f6d4c6c31243c94972b14cd4f43e3e6624e51422
bechir-fahmi <95341647+bechir-fahmi@users.noreply.github.com>
2025-03-21 14:41:35 +0100

    Add UsersModule to encapsulate user-related services, controllers, and entities for better modularity

---
+++ backend/src/modules/users/users.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { User } from './entities/user.entity';
+import { Admin } from './entities/admin.entity';
+import { Accountant } from './entities/accountant.entity';
+import { Finance } from './entities/finance.entity';
+import { FinanceDirector } from './entities/finance-director.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      User,
+      Admin,
+      Accountant,
+      Finance,
+      FinanceDirector,
+    ]),
+  ],
+  providers: [UsersService],
+  controllers: [UsersController],
+  exports: [UsersService],
+})
+export class UsersModule {} 
